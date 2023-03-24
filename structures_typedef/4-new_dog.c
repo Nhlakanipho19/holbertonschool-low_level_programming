97e6ff5586493f3587754446a17c293458e82112
NhlakaniphoSingular <nmsutwana@singular.ac.za>
2023-03-23 23:40:07 -0700

    fixing the beety styles

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -18,12 +18,12 @@ dog_t *new_dog(char *name, float age, char *owner);
  */
 int _strlen(char *str)
 {
-  int len = 0;
+int len = 0;
 
-  while (*str++)
-    len++;
+while (*str++)
+len++;
 
-  return (len);
+return (len);
 }
 
 /**
@@ -36,13 +36,13 @@ int _strlen(char *str)
  */
 char *_strcopy(char *dest, char *src)
 {
-  int index = 0;
-  for (index = 0; src[index]; index++)
-    dest[index] = src[index];
+int index = 0;
+for (index = 0; src[index]; index++)
+dest[index] = src[index];
 
-  dest[index] = '\0';
+dest[index] = '\0';
 
-  return (dest);
+return (dest);
 }
 
 /**
@@ -55,27 +55,27 @@ char *_strcopy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *doggo;
-  if (name == NULL || age < 0 || owner == NULL)
-    return (NULL);
-  doggo = malloc(sizeof(dog_t));
-  if (doggo == NULL)
-    return (NULL);
-  doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-  if (doggo->name == NULL)
-    {
-      free(doggo);
-      return (NULL);
-    }
-  doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-  if (doggo->owner == NULL)
-    {
-      free(doggo->name);
-      free(doggo);
-      return (NULL);
-    }
-  doggo->name = _strcopy(doggo->name, name);
-  doggo->age = age;
-  doggo->owner = _strcopy(doggo->owner, owner);
-  return (doggo);
+dog_t *doggo;
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+return (NULL);
+doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (doggo->name == NULL)
+{
+free(doggo);
+return (NULL);
+}
+doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (doggo->owner == NULL)
+{
+free(doggo->name);
+free(doggo);
+return (NULL);
+}
+doggo->name = _strcopy(doggo->name, name);
+doggo->age = age;
+doggo->owner = _strcopy(doggo->owner, owner);
+return (doggo);
 }
